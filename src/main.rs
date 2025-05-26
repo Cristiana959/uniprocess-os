e2b3e0102ed97bb4363d1c1f5f04ef6efdd6b4be
Cristiana959 <cristiana699@gmail.com>
2025-05-26 22:47:55 +0300

    wip: passing command args

--- src/main.rs
+++ src/main.rs
@@ -2,72 +2,24 @@ use std::io;
 
 use process::create_process;
 mod process;
-enum Command {
-    Greet,
-    Ls,
-    Pwd,
-    Mkdir,
-    Touch,
-    Exit,
-    Unknown,
-}
-
-impl Command {
-    fn from_input(input: &str) -> Self {
-        match input.trim().to_lowercase().as_str() {
-            "greet" => Command::Greet,
-            "ls" => Command::Ls,
-            "pwd" => Command::Pwd,
-            "mkdir" => Command::Mkdir,
-            "touch" => Command::Touch,
-            "exit" => Command::Exit,
-            _ => Command::Unknown,
-        }
-    }
-}
+const COMMAND_PATH: &str = "/Users/cristianaandrei/facultate/uniprocess-os/src/bin";
 
 fn main() {
     loop {
-        println!("Enter a command (greet, ls, exit):");
+        println!("Enter a command (greet, ls, mkdir, touch, exit):");
 
         let mut input = String::new();
         io::stdin()
             .read_line(&mut input)
             .expect("Failed to read input");
 
-        let command = Command::from_input(&input);
-
-        match command {
-            Command::Greet => {
-                println!("Hello there!");
-            }
-            Command::Ls => {
-                create_process(
-                    "/Users/cristianaandrei/facultate/uniprocess-os/src/bin/ls.wasm".to_string(),
-                );
-            }
-            Command::Pwd => {
-                create_process(
-                    "/Users/cristianaandrei/facultate/uniprocess-os/src/bin/pwd.wasm".to_string(),
-                );
-            }
-            Command::Mkdir => {
-                create_process(
-                    "/Users/cristianaandrei/facultate/uniprocess-os/src/bin/mkdir.wasm".to_string(),
-                );
-            }
-            Command::Touch => {
-                create_process(
-                    "/Users/cristianaandrei/facultate/uniprocess-os/src/bin/touch.wasm".to_string(),
-                );
-            }
-            Command::Exit => {
-                println!("Exiting program.");
-                break;
-            }
-            Command::Unknown => {
-                println!("Unknown command.");
-            }
+        let parts: Vec<&str> = input.split_whitespace().collect();
+        let mut cmd_args = "";
+        if parts.len() > 1 {
+            cmd_args = parts[1];
         }
+
+        let cmd = format!("{}/{}.wasm", COMMAND_PATH, parts[0]);
+        create_process(cmd.to_string(), &[cmd_args.to_owned()]);
     }
 }
--- src/process/mod.rs
+++ src/process/mod.rs
@@ -5,40 +5,46 @@ use cap_std::fs::Dir;
 use wasi_common::sync::WasiCtxBuilder;
 use wasmtime::{Engine, Linker, Module, Store};
 
-pub(crate) fn create_process(module_path: String) {
+pub(crate) fn create_process(module_path: String, function_args: &[String]) {
     let engine = Engine::default();
-    let module = Module::from_file(&engine, module_path).unwrap();
+    if let Err(err) = Module::from_file(&engine, module_path.clone()) {
+        println!("Invalid module path {:?}", err);
+    } else {
+        let module = Module::from_file(&engine, module_path).unwrap();
+        let mut linker = Linker::new(&engine);
+        wasi_common::sync::add_to_linker(&mut linker, |t| t).unwrap();
+        // let pre = linker.instantiate_pre(&module).unwrap();
 
-    let mut linker = Linker::new(&engine);
-    wasi_common::sync::add_to_linker(&mut linker, |t| t).unwrap();
-    // let pre = linker.instantiate_pre(&module).unwrap();
-
-    let dir = Dir::open_ambient_dir(
-        "/Users/cristianaandrei/facultate/uniprocess-os/src",
-        ambient_authority(),
-    )
-    .unwrap();
-    let wasi = WasiCtxBuilder::new()
-        .inherit_stdio()
-        .preopened_dir(dir.try_clone().unwrap(), ".")
-        .unwrap()
-        .inherit_args()
-        .unwrap()
-        .build();
-
-    let mut store = Store::new(&engine, wasi);
-
-    let linking1 = linker.instantiate(&mut store, &module).unwrap();
-
-    let run = linking1
-        .get_typed_func::<(), ()>(&mut store, "_start")
+        let dir = Dir::open_ambient_dir(
+            "/Users/cristianaandrei/facultate/uniprocess-os/src",
+            ambient_authority(),
+        )
         .unwrap();
+        let wasi = WasiCtxBuilder::new()
+            .inherit_stdio()
+            .preopened_dir(dir.try_clone().unwrap(), ".")
+            .unwrap()
+            .inherit_args()
+            .unwrap()
+            //.envs(env) //uncomment ca sa pasam env var pentru diretorul de pwd din care taiem partea de mount
+            .args(function_args) //uncomment pentru a pasa argumente functiilor mkdir, touch etc
+            .unwrap()
+            .build();
 
-    let working_directory = Path::new("/Users/cristianaandrei/facultate/uniprocess-os/src"); // The path you want to set as the cwd
-    env::set_current_dir(working_directory).unwrap();
-    println!("Current dir is {:?}", env::current_dir());
-    let _ = thread::spawn(move || {
-        run.call(&mut store, ()).unwrap();
-    })
-    .join();
+        let mut store = Store::new(&engine, wasi);
+
+        let linking1 = linker.instantiate(&mut store, &module).unwrap();
+
+        let run = linking1
+            .get_typed_func::<(), ()>(&mut store, "_start")
+            .unwrap();
+
+        let working_directory = Path::new("/Users/cristianaandrei/facultate/uniprocess-os/src"); // The path you want to set as the cwd
+        env::set_current_dir(working_directory).unwrap();
+        println!("Current dir is {:?}", env::current_dir());
+        let _ = thread::spawn(move || {
+            run.call(&mut store, ()).unwrap();
+        })
+        .join();
+    }
 }
